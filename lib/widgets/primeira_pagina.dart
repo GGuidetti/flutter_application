bbb804a8d88d1b4982ae29d2a6056f56cfd9b02d
joileJr <joilejunior2@gmail.com>
2023-02-17 20:56:48 -0300

    checkpoint

--- lib/widgets/primeira_pagina.dart
+++ lib/widgets/primeira_pagina.dart
@@ -3,19 +3,32 @@ import 'package:flutter/material.dart';
 
 class PrimeiraPagina extends StatelessWidget {
   @override
+  var valorInput;
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        //Definindo o titulo da home
         title: Text('HomePage do Guidera 2'),
       ),
       body: Center(
           child: Column(
         children: [
           TextField(
-            decoration: InputDecoration(label: Text("Nome: "),
-            hintText: "Informe seu nome"),
-          ),
-          ElevatedButton(onPressed: () {}, child: Text("ok"))
+              //Criando um input com Label e placeholder,
+              //que ao ser alterado printa o valor do input no console
+              decoration: InputDecoration(
+                  label: Text("Nome: "), hintText: "Informe seu nome"),
+              onChanged: (valorDigitado) {
+                valorInput = valorDigitado;
+                print(valorDigitado);
+              }),
+          //Criando um botão flutuante que ao ser pressionado
+          //printa o valor do input no console 
+          ElevatedButton(
+              onPressed: () {
+                print(valorInput);
+              },
+              child: Text("ok"))
         ],
       )),
     );
